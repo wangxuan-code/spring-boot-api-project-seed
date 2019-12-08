4cac8205d7510672ab58530a3e31b9bd61d617ad
asherwang <asherwang@vip.qq.com>
2019-12-08 21:30:41 +0800

    初始提交

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -3,6 +3,7 @@ package com.company.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//启动任务
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
